e060756bdda74de8510d19a3f268ebb752b5bfd5
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-01-07 23:39:26 +0100

    Updated Month Sale Component

--- frontend/src/Components/MonthSale.jsx
+++ frontend/src/Components/MonthSale.jsx
@@ -6,7 +6,9 @@ const MonthSale = () => {
       style={{ background: "url(/video-placeholder.png)" }}
       className="h-[280px] w-full max-w-screen-xl bg-slate-600 bg-cover bg-no-repeat mx-auto flex flex-col justify-center items-center gap-3"
     >
-      <h2 className="text-5xl text-white font-extrabold">SALE OF THE MONTH</h2>
+      <h2 className="md:text-5xl sm:text-2xl text-lg text-center text-white font-extrabold">
+        SALE OF THE MONTH
+      </h2>
 
       {/* Scale Up the Component  */}
       <CountDown shop={true} />
